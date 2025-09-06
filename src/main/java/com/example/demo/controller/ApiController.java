7e2995783a2d9eb9c85e800fbc1128fb0e37d6e2
swson <swson@foodtechkorea.com>
2025-09-06 13:19:56 +0900

    Post Method Api add

--- src/main/java/com/example/demo/controller/ApiController.java
+++ src/main/java/com/example/demo/controller/ApiController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class ApiController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/{id}")
+    public String hello(@PathVariable String id) {
+        return "Hello " + id;
     }
 
 }
